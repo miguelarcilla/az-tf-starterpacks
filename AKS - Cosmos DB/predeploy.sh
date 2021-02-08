84b4f687c86fea1400830331d87033eaa26a750e
Miguel Arcilla <arcilla.miguel@gmail.com>
2021-02-08 12:23:31 +0800

    Added predeploy ref for aks cli reqs

---
+++ AKS - Cosmos DB/predeploy.sh
@@ -0,0 +1,12 @@
+# Pre-requisites
+az feature register --name EnablePodIdentityPreview --namespace Microsoft.ContainerService
+az provider register -n Microsoft.ContainerService
+az extension add --name aks-preview
+az extension update --name aks-preview
+
+az feature register --name AKS-IngressApplicationGatewayAddon --namespace Microsoft.ContainerService
+az provider register --namespace Microsoft.ContainerService
+
+cd dotnet/WeatherShare
+docker build -t weather-share:latest .
+docker image tag weather-share:latest ACR_NAME.azurecr.io/weather-share:latest # Change
