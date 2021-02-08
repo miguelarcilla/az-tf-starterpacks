ae3667f119ae5a5d74c27ddc1e605a306ca2d7b7
Miguel Arcilla <arcilla.miguel@gmail.com>
2021-02-08 12:23:53 +0800

    Genericized and shortened post-deploy cmds

--- AKS - Cosmos DB/postdeploy.sh
+++ AKS - Cosmos DB/postdeploy.sh
@@ -1,17 +1,3 @@
-az acr login -n pbzxacr
-docker push pbzxacr.azurecr.io/weather-share
-az aks Get-Credentials -a --name pbzx-aks -g pbzx-rg
-
-kubectl create -f https://raw.githubusercontent.com/Azure/aad-pod-identity/master/deploy/infra/deployment.yaml
-kubectl apply -f https://raw.githubusercontent.com/Azure/aad-pod-identity/v1.7.1/deploy/infra/mic-exception.yaml
-
-# kubectl create serviceaccount --namespace kube-system tiller-sa
-# kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller-sa
-
-helm repo add aad-pod-identity https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts
-helm install aad-pod-identity aad-pod-identity/aad-pod-identity
-helm repo add application-gateway-kubernetes-ingress https://appgwingress.blob.core.windows.net/ingress-azure-helm-package/
-helm repo update
-
-helm install -f aks/helm-config.yaml application-gateway-kubernetes-ingress/ingress-azure --generate-name
-kubectl apply -f aks/weather-share.yaml
+az acr login -n ACR_NAME # Change
+docker push ACR_NAME.azurecr.io/weather-share # Change
+az aks Get-Credentials -a --name AKS_CLUSTER_NAME -g RESOURCE_GROUP_NAME # Change
