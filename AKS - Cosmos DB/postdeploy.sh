e996b5d508496880e3b50d7a90fbce1a06b76c1d
Miguel Arcilla <arcilla.miguel@gmail.com>
2021-02-19 09:11:33 +0800

    Added K8s cmd to postdeploy

--- AKS - Cosmos DB/postdeploy.sh
+++ AKS - Cosmos DB/postdeploy.sh
@@ -1,3 +1,4 @@
 az acr login -n ACR_NAME # Change
 docker push ACR_NAME.azurecr.io/weather-share # Change
 az aks Get-Credentials -a --name AKS_CLUSTER_NAME -g RESOURCE_GROUP_NAME # Change
+kubectl apply -f aks/weather-share.yaml --namespace weather-share
