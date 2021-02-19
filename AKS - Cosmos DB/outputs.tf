4287c893a3ab0faec9b3a5afe9b069cd0f02da2b
Miguel Arcilla <arcilla.miguel@gmail.com>
2021-02-19 10:42:42 +0800

    partial usability update to outputs

--- AKS - Cosmos DB/outputs.tf
+++ AKS - Cosmos DB/outputs.tf
@@ -6,14 +6,26 @@
 # and include any data from the state file. Outputs are sorted alphabetically;
 # use an underscore _ to move things to the bottom. 
 
-output "_instructions" {
-  value = "This output contains plain text. You can add variables too."
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
 }
 
-output "kubelet_identity_client_id" {
-    value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].client_id
+output "acr_uri" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "keyvault_uri" {
+  value = azurerm_key_vault.keyvault.vault_uri
 }
 
 output "aks_node_resource_group" {
-    value = azurerm_kubernetes_cluster.aks.node_resource_group
+  value = azurerm_kubernetes_cluster.aks.node_resource_group
+}
+
+output "appgw_public_ip_address" {
+  value = azurerm_public_ip.appgw_ip.ip_address
+}
+
+output "appgw_public_ip_fqdn" {
+  value = azurerm_public_ip.appgw_ip.fqdn
 }
